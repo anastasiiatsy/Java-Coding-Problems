af7f806d353696d0c3b45d99166db240e1d59f64
Anastasiia Tsygankova <anastasiiat@hotmail.com>
2024-10-14 19:14:16 +0100

    Java Coding Problems

---
+++ String, Numbers and Math/Count-Substring-Occurrences-In-A-String/src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(numOfOccurrences("helloslkhellodjladfjhello" ,
+                "hello"));
+    }
+    /*
+    String String -> Integer
+    counts the occurrences of a given substring in a given string
+    "dhimanman" "man"                     -> 2
+    "helloslkhellodjladfjhello" "hello"   -> 3
+    "" ""                                 -> IllegalArgumentException
+    null null                             -> IllegalArgumentException
+     */
+
+    public static int numOfOccurrences(String string, String substring) {
+        if (string == null || substring == null || string.isBlank() || substring.isBlank()) {
+            throw new IllegalArgumentException("Invalid string");
+        }
+
+        String newString = string.replace(substring, "");
+
+        int num = (string.length() - newString.length()) / substring.length();
+
+        return num;
+
+    }
+}
