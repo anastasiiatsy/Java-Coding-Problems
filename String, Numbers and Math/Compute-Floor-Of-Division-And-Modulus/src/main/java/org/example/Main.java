407773aa1f145eb5651c607368431900c74177d4
Anastasiia Tsygankova <anastasiiat@hotmail.com>
2024-10-23 21:47:51 +0100

    Java Coding Problems

---
+++ String, Numbers and Math/Compute-Floor-Of-Division-And-Modulus/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Math.floorDiv(-45, 4));
+        System.out.println(Math.floorMod(-45, 4));
+    }
+
+    /*
+    int int -> int
+    computes the floor division of the given dividend (x) and divisor (y)
+     */
+
+    /*
+    int int -> int
+    computes the floor modulus of the given dividend (x) and divisor (y)
+     */
+}
