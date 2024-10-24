b98ff3ed53d5a2d7eee1b4d18d9c143a99f99adc
Anastasiia Tsygankova <anastasiiat@hotmail.com>
2024-10-24 21:13:12 +0100

    Java Coding Problems

---
+++ String, Numbers and Math/Multiply-Two-Large-int-long-Values/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Math.multiplyExact(234, 2345));
+    }
+    /*
+    long long -> long
+    multiplies two large long values and throws an arithmetic exception in the case of operation overflow
+     */
+}
