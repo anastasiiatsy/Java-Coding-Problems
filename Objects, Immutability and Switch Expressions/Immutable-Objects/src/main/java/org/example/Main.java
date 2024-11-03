11a622e07fe3a1d27e5aaf062ebc97f54f623286
Anastasiia Tsygankova <anastasiiat@hotmail.com>
2024-11-03 18:10:13 +0000

    Java Coding Problems

---
+++ Objects, Immutability and Switch Expressions/Immutable-Objects/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Example of immutable objects
+        String cat = "Garfield";
+        final int days = 15;
+        final List<String> names = new ArrayList<>();
+    }
+}
